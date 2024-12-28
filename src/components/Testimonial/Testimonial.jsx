58d71966673f83d55572628535f12eeb4830393d
Rutuja1104 <rutujasahae2018@gmail.com>
2024-12-28 16:13:35 +0530

    added ui chnages

---
+++ src/components/Testimonial/Testimonial.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+
+const testimonialData = [
+  {
+    name: "John",
+    image: "",
+    description: "Drive electric and enjoy efficiency, sustainability, and cutting-edge technology in every journey.",
+    aosDelay: "0",
+  },
+  {
+    name: "Smith",
+    image: "",
+    description: "Electric vehicles (EVs) offer a cleaner, quieter, and more efficient way to drive. With zero emissions and lower fuel costs, they’re the smart choice for a sustainable future.",
+    aosDelay: "300",
+  },
+  {
+    name: "Allen",
+    image: "",
+    description: "Save on fuel costs, enjoy a smooth, quiet ride, and reduce your carbon footprint. EVs offer cutting-edge technology, zero emissions, and a sustainable lifestyle choice that benefits both you and the planet",
+    aosDelay: "1000",
+  },
+];
+const Testimonial = () => {
+  return (
+    <>
+      <span id="about"></span>
+      <div className="dark:bg-black dark:text-white py-14 sm:pb-24">
+        <div className="container">
+          {/* Header */}
+          <div className="space-y-4 pb-12">
+            <p
+              data-aos="fade-up"
+              className="text-3xl font-semibold text-center sm:text-4xl font-serif"
+            >
+              What Our Clients Say About Us
+            </p>
+            <p data-aos="fade-up" className="text-center sm:px-44">
+            Join the Revolution: Electric Vehicles for a Sustainable Tomorrow
+            </p>
+          </div>
+
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 text-black dark:text-white">
+            {testimonialData.map((skill) => (
+              <div
+                key={skill.name}
+                data-aos="fade-up"
+                data-aos-delay={skill.aosDelay}
+                className="card text-center group space-y-3 sm:space-y-6 p-4 sm:py-12 dark:bg-white/20 bg-gray-100 duration-300  rounded-lg "
+              >
+                <div className="grid place-items-center ">
+                  <img
+                    src="https://picsum.photos/200"
+                    alt=""
+                    className="rounded-full w-20 h-20"
+                  />
+                </div>
+                <div className="text-2xl">⭐⭐⭐⭐⭐</div>
+                <p>{skill.description}</p>
+                <p className="text-center font-semibold">{skill.name}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Testimonial;
