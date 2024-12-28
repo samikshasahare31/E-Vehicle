a0cf6284886e7a222a0fe88e8270120cac65d30f
samikshasahare31 <samikshasahare31@gmail.com>
2024-12-28 16:38:47 +0530

    added chage about page

---
+++ src/components/CarList/CarList.jsx
@@ -0,0 +1,103 @@
+import React from "react";
+import whiteCar from "../../assets/white-car.png";
+import car2 from "../../assets/car5.png";
+import car3 from "../../assets/car6.png";
+import car4 from "../../assets/car3.jpg";
+import car5 from "../../assets/car4.png";
+import car6 from "../../assets/car7.jpg";
+
+const carList = [
+  {
+    name: "BMW UX",
+    price: 100,
+    image: whiteCar,
+    aosDelay: "0",
+  },
+  {
+    name: "KIA UX",
+    price: 140,
+    image: car2,
+    aosDelay: "500",
+  },
+  {
+    name: "BMW UX",
+    price: 100,
+    image: car3,
+    aosDelay: "1000",
+  },
+  {
+    name: "BMW UX",
+    price: 100,
+    image: car4,
+    aosDelay: "1000",
+  },
+  {
+    name: "BMW UX",
+    price: 100,
+    image: car5,
+    aosDelay: "1000",
+  },
+  {
+    name: "BMW UX",
+    price: 100,
+    image: car6,
+    aosDelay: "1000",
+  },
+];
+
+const CarList = () => {
+  return (
+    <div className="pb-24">
+      <div className="container">
+        {/* Heading */}
+        <h1
+          data-aos="fade-up"
+          className="text-3xl sm:text-4xl font-semibold font-serif mb-3"
+        >
+          Drive Into the Future: Go Green with Electric Vehicles!
+        </h1>
+        <p data-aos="fade-up" aos-delay="400" className="text-sm pb-10">
+        Save Money, Save the Planet: Switch to Electric Today.
+        </p>
+        {/* Car listing */}
+        <div>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-16">
+            {carList.map((data) => (
+              <div
+                data-aos="fade-up"
+                data-aos-delay={data.aosDelay}
+                className="space-y-3 border-2 border-gray-300 hover:border-primary p-3 rounded-xl relative group"
+              >
+                <div className="w-full h-[120px]">
+                  <img
+                    src={data.image}
+                    alt=""
+                    className="w-full h-[120px] object-contain sm:translate-x-8 group-hover:sm:translate-x-16 duration-700"
+                  />
+                </div>
+                <div className="space-y-2">
+                  <h1 className="text-primary font-semibold">{data.name}</h1>
+                  <div className="flex justify-between items-center text-xl font-semibold">
+                    <p>${data.price}/Day</p>
+                    <a href="#">Details</a>
+                  </div>
+                </div>
+                <p className="text-xl font-semibold absolute top-0 left-3">
+                  12Km
+                </p>
+              </div>
+            ))}
+          </div>
+        </div>
+        {/* End of car listing */}
+        <div className="grid place-items-center mt-8">
+          <button data-aos="fade-up" className="button-outline">
+            Get Started
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CarList;
