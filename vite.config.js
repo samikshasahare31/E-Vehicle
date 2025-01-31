c5f999d70c5f08e3211650253d36fa064dbfd375
samikshasahare31 <149174101+samikshasahare31@users.noreply.github.com>
2025-01-31 20:11:26 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/EV-INSIGHT-HUB/', // Add this line
+  base: '/E-Vehicle/', // Add this line
 });
